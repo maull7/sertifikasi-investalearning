890cf554afd73dbd6006a66b68478338771ac17c
maull7 <rmgaming601@gmail.com>
2026-01-17 19:55:19 +0700

    admin user

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::updateOrCreate(
+            [
+                'email' => 'admin@example.com',
+            ],
+            [
+                'name' => 'Administrator',
+                'password' => Hash::make('12345678'),
+                'role' => 'Admin',
+            ]
+        );
+    }
+}
